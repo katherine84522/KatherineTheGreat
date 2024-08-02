8bf16924afd608ce15e830896fe76dbb9cb7df1e
Katherine Tsai <115205162+katherine84522@users.noreply.github.com>
2024-08-01 18:48:35 -0700

    deployed successfully to sepolia

--- ignition/modules/MyERC20Token.js
+++ ignition/modules/MyERC20Token.js
@@ -1,26 +1,34 @@
-const hre = require("hardhat");
-
-async function main(){
-
-    const [deployer] = await ethers.getSigners();
-
-    // console.log("Deploying contract with account: ", deployer.address);
-    // console.log("Account balance: ", (await deployer.getBalance()).toString());
-
-    const KatherineTheGreat = await hre.ethers.getContractFactory("MyERC20Token");
-    const token = await KatherineTheGreat.deploy(5000000000);
-
-    await token.deployed();
-
-    console.log("Token address: ", token.address);
-
-    await hre.run("verify:verify", {
-        address: token.address,
-        constructorArguments: [5000000000]
-    })
-}
-
-main().catch((error)=>{
-    console.error(error);
-    process.exitCode = 1;
-})
+// ./ignition/modules/Token.ts 
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+ 
+/* 
+ The callback passed to `buildModule()` provides a module builder object `m` 
+ as a parameter. Through this builder object, you access the Module API. 
+ For instance, you can deploy contracts via `m.contract()`. 
+*/ 
+module.exports = buildModule("TokenModule", (m) => { 
+ /*  
+  Instead of named accounts, you get access to the configured accounts 
+  through the `getAccount()` method. 
+ */ 
+  const deployer = m.getAccount(0); 
+  const tokenOwner = m.getAccount(0); 
+ 
+ /* 
+  Deploy `Token` by calling `contract()` with the constructor arguments 
+  as the second argument. The account to use for the deployment transaction 
+  is set through `from` in the third argument, which is an options object. 
+ */ 
+  const token = m.contract("MyERC20Token", [tokenOwner], { 
+    from: deployer, 
+  }); 
+ 
+ /* 
+  The call to `m.contract()` returns a future that can be used in other `m.contract()` 
+  calls (e.g. as a constructor argument, where the future will resolve to the 
+  deployed address), but it can also be returned from the module. Contract 
+  futures that are returned from the module can be leveraged in Hardhat tests 
+  and scripts, as will be shown later. 
+ */ 
+  return { token }; 
+});
